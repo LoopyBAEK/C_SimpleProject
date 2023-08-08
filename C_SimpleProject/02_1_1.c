f30b43b80d0bdad8df310aa02686da98ad4dca26
Loopy_BAEK <likipop@naver.com>
2023-08-08 12:34:09 +0900

    문제 02-1 [예제 변경해보기] 문제1, 문제2

---
+++ C_SimpleProject/02_1_1.c
@@ -0,0 +1,7 @@
+// 문제 02-1 [예제 변경해보기] 문제1
+#include <stdio.h>
+
+int main(void) {
+	printf("홍길동\n홍 길 동\n홍  길  동");
+	return 0;
+}
---
+++ C_SimpleProject/02_1_2.c
@@ -0,0 +1,8 @@
+// 문제 02-1 [예제 변경해보기] 문제2
+#include <stdio.h>
+
+int main() {
+	printf("홍길동\n");
+	printf("경상북도 00시 00동\n");
+	printf("010-0000-0000\n");
+}
