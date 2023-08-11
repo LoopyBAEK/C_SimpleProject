bcb437f5cb397e5eaf996ad398f42eaa848c75a6
Loopy_BAEK <likipop@naver.com>
2023-08-11 14:08:02 +0900

    Chapter06 printf 예제

---
+++ C_SimpleProject/StringPrintf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("I like programming \n");
+	printf("I love puppy! \n");
+	printf("I am so happy \n");
+	printf("¾ÕÁý °­¾ÆÁö°¡ ¸»Çß´Ù. \"¸Û~! ¸Û~!\" Á¤¸» ±Í¿±´Ù.");
+	return 0;
+}
