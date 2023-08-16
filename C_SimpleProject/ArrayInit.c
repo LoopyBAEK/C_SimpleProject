d13afd672245d4229dc1b15df79acef93b4d69cf
Loopy_BAEK <likipop@naver.com>
2023-08-16 11:43:09 +0900

    Chapter11 배열 길이 계산

---
+++ C_SimpleProject/ArrayInit.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+	int arr1[5] = { 1, 2, 3, 4, 5 };
+	int arr2[] = { 1, 2, 3, 4, 5, 6 ,7 };
+	int arr3[5] = { 1, 2 };
+	int ar1Len, ar2Len, ar3Len, i;
+
+	printf("배열 arr1의 크기: %d \n", sizeof(arr1));	// 배열의 이름을 대상으로 하는 sizeof 연산의 결과로는 '바이트 단위의 배열 크기'가 반환된다.
+	printf("배열 arr2의 크기: %d \n", sizeof(arr2));
+	printf("배열 arr3의 크기: %d \n", sizeof(arr3));
+
+	ar1Len = sizeof(arr1) / sizeof(int);	// 배열 arr1의 길이 계산
+	ar2Len = sizeof(arr2) / sizeof(int);	// 배열 arr2의 길이 계산
+	ar3Len = sizeof(arr3) / sizeof(int);	// 배열 arr3의 길이 계산
+
+	for (i = 0; i < ar1Len; i++) {
+		printf("%d ", arr1[i]);
+	}
+	printf("\n");
+
+	for (i = 0; i < ar2Len; i++) {
+		printf("%d ", arr2[i]);
+	}
+	printf("\n");
+
+	for (i = 0; i < ar3Len; i++) {
+		printf("%d ", arr3[i]);
+	}
+	printf("\n");
+
+	return 0;
+}
