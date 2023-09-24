0b2139c58efdb39a5f94d67f3b9f38a9b407bbec
Loopy_BAEK <likipop@naver.com>
2023-09-24 17:06:41 +0900

    Chatper24 파일에 데이터 써보기

---
+++ C_SimpleProject/FirstFileWrite.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main() {
+	FILE* fp = fopen("data.txt", "wt");
+	// 경로를 지정해주고 싶다면 경로를 포함해서 파일의 이름을 지정해도 된다.
+	if (fp == NULL) {
+		puts("파일 오픈 실패!");
+		return -1;	// 비정상적 종료를 의미하기 위해서 -1을 반환
+	}
+
+	fputc('A', fp);
+	fputc('B', fp);
+	fputc('C', fp);
+	fclose(fp);	// 스트림의 종료
+	return 0;
+}
